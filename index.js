d96620495a1f7ad8f71244bf699c26513fc3b991
meghna-saxena <meghna.s1906@gmail.com>
2017-10-05 17:32:36 +0530

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//Main starting poit of the application
+//Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+//App Setup
+
+
+
+
+//Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening to:', port);
