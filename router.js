4af9d51556ced1b3cbf2fa99944331679aefc3d6
meghna-saxena <meghna.s1906@gmail.com>
2017-10-06 20:33:29 +0530

    authentication controller

--- router.js
+++ router.js
@@ -1,8 +1,7 @@
+const Authentication = require('./controllers/authentication');
+
 //Access app in the function
 module.exports = function(app) {
     //define a route that user can visit
-    app.get ('/', function (req, res, next) {
-        //send a response of some JSON to incoming http request
-        res.send (['waterbottle', 'phone', 'paper']);
-    });
+    app.post('/signup', Authentication.signup);
 }
