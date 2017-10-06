05f0ac3a498c017bdcee4a26a11e6e25afa554cb
meghna-saxena <meghna.s1906@gmail.com>
2017-10-06 22:57:52 +0530

    searching for given email from Users model

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,19 @@
+const User  = require('../models/user');
+
 exports.signup = function(req, res, next) {
-    res.send({success: 'true'});
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    //See if a user with given email exists
+    User.findOne({ email: email}, function(err, existingUser){
+
+    });
+
+    //If a user with email exists, return an error
+
+
+    //If a user with email doesnt exist, create & save record
+
+    
+    //Respond to request indicating the user was created
 }
