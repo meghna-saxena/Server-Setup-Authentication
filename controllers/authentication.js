19fc77f4732ea965bf5e7ab02acc3f1fc946bf68
meghna-saxena <meghna.s1906@gmail.com>
2017-10-07 12:37:44 +0530

    creating user records

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -6,14 +6,23 @@ exports.signup = function(req, res, next) {
     
     //See if a user with given email exists
     User.findOne({ email: email}, function(err, existingUser){
+        if(err) {return next(err); }
 
+         //If a user with email exists, return an error
+         if(existingUser) {
+             return res.status(422).send({error: 'Email is in use'});
+         }
+
+         //If a user with email doesnt exist, create & save record
+         const user = new User ({
+             email: email,
+             password: password
+         });
+         user.save(function(err) {
+             if(err) {return next(err); }
+
+          //Respond to request indicating the user was created
+          res.json({success: true});
+        });
     });
-
-    //If a user with email exists, return an error
-
-
-    //If a user with email doesnt exist, create & save record
-
-    
-    //Respond to request indicating the user was created
 }
