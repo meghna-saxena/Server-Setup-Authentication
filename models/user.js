58a8b076751066698221db9960db5484cfb33016
meghna-saxena <meghna.s1906@gmail.com>
2017-10-07 22:48:28 +0530

    encrypting passwords - added comments

--- models/user.js
+++ models/user.js
@@ -9,16 +9,23 @@ const userSchema = new Schema ({
 });
 
 //On Save Hook, encrypt Password
+//Before saving a model, run this function
 userSchema.pre('save', function(next) {
+    //get access to user model, 
+    //here user is instance of user model
     const user = this;
 
+    //Generate a salt, and then run a callback
     bcrypt.genSalt(10, function(err, salt) {
         if(err) {return next(err); }
 
+        //hash(encrypt) the password using salt
         bcrypt.hash(user.password, salt, null, function(err, hash) {
             if(err) {return next(err); }
 
+            //Overwrite plain text password with encrypted password
             user.password = hash;
+            //save the model
             next();
         });
     });
