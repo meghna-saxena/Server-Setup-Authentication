5b9fa680288cf744b34f637a020519ad1413230e
meghna-saxena <meghna.s1906@gmail.com>
2017-10-09 21:05:13 +0530

    finished signin

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,6 +7,12 @@ function tokenForUser(user) {
     return jwt.encode({sub: user.id, iat: timestamp}, config.secret);
 }
 
+exports.signin = function(req, res, next) {
+    //User has email and password auth'd
+    //provide a token to them
+    res.send({token: tokenForUser(req.user)});
+}
+
 exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
