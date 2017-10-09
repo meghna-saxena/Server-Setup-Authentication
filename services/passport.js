14928cfdae5bccdd42ef1532e02c8cd017eeee0e
meghna-saxena <meghna.s1906@gmail.com>
2017-10-09 17:54:15 +0530

    using strategies with passport

--- services/passport.js
+++ services/passport.js
@@ -5,7 +5,10 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 //Setup options for jwt strategy
-const jwtOptions = {} 
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+} ;
 
 //Create jwt strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
@@ -24,5 +27,5 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
     });
 });
 
-
 //Tell passport to use this strategy
+passport.use(jwtLogin);
