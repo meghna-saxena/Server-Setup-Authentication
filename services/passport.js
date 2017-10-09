99dab96ee04b84ffd04d2dd52faebb915817061b
meghna-saxena <meghna.s1906@gmail.com>
2017-10-09 17:39:43 +0530

    configuring passport strategies

--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,26 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+//Setup options for jwt strategy
+const jwtOptions = {} 
+
+//Create jwt strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+    //See if user ID in payload exists in database
+    //If it does, call 'done' with that user,
+    //otherwise, call 'done' without user object
+    //Note: user ID is encoded as sub property on the token
+
+    User.findByID(payload.sub, function(err, user) {
+        if(err) {return done(err, false); }
+        if(user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
+});
+
+
+//Tell passport to use this strategy
