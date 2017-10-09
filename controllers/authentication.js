7aae47e91f3ab4c8e9fd5de09ab6f96a6fef7c9b
meghna-saxena <meghna.s1906@gmail.com>
2017-10-09 11:38:52 +0530

    created a jwt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
 const User  = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({sub: user.id, iat: timestamp}, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -25,8 +32,8 @@ exports.signup = function(req, res, next) {
          user.save(function(err) {
              if(err) {return next(err); }
 
-          //Respond to request indicating the user was created
-          res.json({success: true});
+          //respond with identifying piece of info (jwt)
+          res.json({token: tokenForUser(user)});
         });
     });
 }
